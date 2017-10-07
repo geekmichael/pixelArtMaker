f4e6978d22aaf902da14e2f83883691f8be0b7a3
Michael ZHANG <geek.michael@live.com>
2017-10-07 15:09:57 +0800

    Updated core functions

--- designs.js
+++ designs.js
@@ -1,10 +1,27 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
+    var table = document.getElementById('pixel_canvas');
+    var rows = document.getElementById('input_height').value;
+    var columns = document.getElementById('input_width').value;
 
-// Your code goes here!
-
+    var i, j, row, column;
+    for (i = 0; i < rows; i++) {
+        row = table.insertRow();
+        for (j = 0; j < columns; j++) {
+            column = row.insertCell();
+            column.addEventListener('click', toggleBgcolor, false);
+        }
+    }
+    console.log(rows, columns, table);
 }
+
+function toggleBgcolor(e){
+    var bgcolor = document.getElementById('colorPicker').value;
+    var activeBg = this.style.backgroundColor;
+    this.style.backgroundColor = activeBg ? "" : bgcolor;
+}
+
+document.getElementById('sizePicker').onsubmit= function(e){
+    e.preventDefault();
+};
+
+document.getElementById('submit').addEventListener('click', makeGrid, false);
